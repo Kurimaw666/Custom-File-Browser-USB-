1554790d50ea0ffc94a75659d552b506f3aaedf7
Kurimaw666 <112299533+Kurimaw666@users.noreply.github.com>
2022-11-02 09:23:25 +0800

    v1
    current Implementation of custom file browser using winfom

---
+++ fileBrower/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace fileBrower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Icon_list = new System.Windows.Forms.ImageList(this.components);
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.File_dt_time = new System.Windows.Forms.Timer(this.components);
+            this.File_path_tb = new MaterialSkin.Controls.MaterialTextBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.back_bt = new MaterialSkin.Controls.MaterialButton();
+            this.Done = new MaterialSkin.Controls.MaterialButton();
+            this.SuspendLayout();
+            // 
+            // Icon_list
+            // 
+            this.Icon_list.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("Icon_list.ImageStream")));
+            this.Icon_list.TransparentColor = System.Drawing.Color.Transparent;
+            this.Icon_list.Images.SetKeyName(0, "pdf-icon-3.png");
+            this.Icon_list.Images.SetKeyName(1, "oldword.png");
+            this.Icon_list.Images.SetKeyName(2, "word.png");
+            this.Icon_list.Images.SetKeyName(3, "folder.png");
+            this.Icon_list.Images.SetKeyName(4, "unknowFile.png");
+            this.Icon_list.Images.SetKeyName(5, "folder-invoices.png");
+            this.Icon_list.Images.SetKeyName(6, "wordi.ico");
+            // 
+            // listView3
+            // 
+            this.listView3.AllowDrop = true;
+            this.listView3.CausesValidation = false;
+            this.listView3.HideSelection = false;
+            this.listView3.LargeImageList = this.Icon_list;
+            this.listView3.Location = new System.Drawing.Point(545, 93);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(243, 349);
+            this.listView3.SmallImageList = this.Icon_list;
+            this.listView3.TabIndex = 3;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView3_DragDrop);
+            this.listView3.DragOver += new System.Windows.Forms.DragEventHandler(this.listView3_DragOver);
+            // 
+            // File_dt_time
+            // 
+            this.File_dt_time.Interval = 1000;
+            this.File_dt_time.Tick += new System.EventHandler(this.File_dt_time_Tick);
+            // 
+            // File_path_tb
+            // 
+            this.File_path_tb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.File_path_tb.Depth = 0;
+            this.File_path_tb.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.File_path_tb.LeadingIcon = null;
+            this.File_path_tb.Location = new System.Drawing.Point(34, 83);
+            this.File_path_tb.MaxLength = 50;
+            this.File_path_tb.MouseState = MaterialSkin.MouseState.OUT;
+            this.File_path_tb.Multiline = false;
+            this.File_path_tb.Name = "File_path_tb";
+            this.File_path_tb.Size = new System.Drawing.Size(494, 50);
+            this.File_path_tb.TabIndex = 10;
+            this.File_path_tb.Text = "";
+            this.File_path_tb.TrailingIcon = null;
+            // 
+            // listView2
+            // 
+            this.listView2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.listView2.HideSelection = false;
+            this.listView2.LargeImageList = this.Icon_list;
+            this.listView2.Location = new System.Drawing.Point(34, 139);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(494, 303);
+            this.listView2.TabIndex = 11;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.listView2_ItemDrag);
+            this.listView2.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView2_ItemSelectionChanged);
+            this.listView2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView2_MouseDoubleClick);
+            // 
+            // back_bt
+            // 
+            this.back_bt.AutoSize = false;
+            this.back_bt.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.back_bt.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.back_bt.Depth = 0;
+            this.back_bt.HighEmphasis = true;
+            this.back_bt.Icon = null;
+            this.back_bt.Location = new System.Drawing.Point(455, 448);
+            this.back_bt.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.back_bt.MouseState = MaterialSkin.MouseState.HOVER;
+            this.back_bt.Name = "back_bt";
+            this.back_bt.Size = new System.Drawing.Size(73, 29);
+            this.back_bt.TabIndex = 12;
+            this.back_bt.Text = "<<";
+            this.back_bt.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.back_bt.UseAccentColor = false;
+            this.back_bt.UseVisualStyleBackColor = true;
+            this.back_bt.Click += new System.EventHandler(this.back_bt_Click);
+            // 
+            // Done
+            // 
+            this.Done.AutoSize = false;
+            this.Done.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Done.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.Done.Depth = 0;
+            this.Done.HighEmphasis = true;
+            this.Done.Icon = null;
+            this.Done.Location = new System.Drawing.Point(715, 448);
+            this.Done.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Done.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Done.Name = "Done";
+            this.Done.Size = new System.Drawing.Size(73, 29);
+            this.Done.TabIndex = 13;
+            this.Done.Text = "<<";
+            this.Done.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.Done.UseAccentColor = false;
+            this.Done.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 482);
+            this.Controls.Add(this.Done);
+            this.Controls.Add(this.back_bt);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.File_path_tb);
+            this.Controls.Add(this.listView3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList Icon_list;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Timer File_dt_time;
+        private MaterialSkin.Controls.MaterialTextBox File_path_tb;
+        private System.Windows.Forms.ListView listView2;
+        private MaterialSkin.Controls.MaterialButton back_bt;
+        private MaterialSkin.Controls.MaterialButton Done;
+    }
+}
+
---
+++ fileBrower/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+
+namespace fileBrower
+{
+    public partial class Form1 : MaterialForm
+    {
+        
+        private bool isfile= false;
+        private string currentSelecteditemName = "";
+        private bool newder;
+        public Form1()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+
+        }
+
+        private void File_dt_time_Tick(object sender, EventArgs e)
+        {
+            if (ListRemovable())
+            {
+                Debug.WriteLine("detected");
+
+            }
+            else
+            {
+                Debug.WriteLine("No Drive");
+            }
+
+        }
+
+        public void loadfilesAction()
+        {
+            loadfilesdirectories();
+            isfile = false;
+        }
+        private void loadfilesdirectories()
+        {
+            string filepath = File_path_tb.Text;
+            DirectoryInfo fileList;
+            FileAttributes fileAttr;
+            try
+            {
+                if (isfile)
+                {
+                    fileAttr = File.GetAttributes(filepath);
+                }
+                else
+                {
+                    fileAttr = File.GetAttributes(filepath);
+                    
+                }
+                if (((fileAttr & FileAttributes.Directory) == FileAttributes.Directory) & !isfile)
+                {
+
+                    fileList = new DirectoryInfo(filepath + "/" + currentSelecteditemName);
+                    File_path_tb.Text = fileList.ToString();
+                    FileInfo[] files = fileList.GetFiles();
+                    DirectoryInfo[] dirs = fileList.GetDirectories();
+                    listView2.Clear();
+                    for (int i = 0; i < dirs.Length; i++)
+                    {
+
+                        listView2.Items.Add(dirs[i].Name, 5);
+                    }
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        string extention = Path.GetExtension(files[i].Name);
+
+                        switch (extention)
+                        {
+                            case ".pdf":
+                                listView2.Items.Add(files[i].Name, 0);
+                                break;
+                            case ".docx":
+                                listView2.Items.Add(files[i].Name, 6);
+                                break;
+                        }
+                    }
+                    currentSelecteditemName = "";
+                }
+
+               
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+        }
+
+
+        private bool ListRemovable()
+        {
+
+            foreach (DriveInfo d in DriveInfo.GetDrives())
+            {
+                if (d.IsReady && d.DriveType == DriveType.Removable)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        private string getDrivePath()
+        {
+            foreach (DriveInfo d in DriveInfo.GetDrives())
+            {
+                if (d.IsReady && d.DriveType == DriveType.Removable)
+                {
+                    string root = d.RootDirectory.Name;
+                    root = root.Remove(2, 1);
+                    return root;
+                }
+            }
+            return "";
+        }
+
+        private void listView2_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            try
+            {
+                string filepath = File_path_tb.Text;
+                currentSelecteditemName = e.Item.Text;
+                Debug.WriteLine(currentSelecteditemName);
+                FileAttributes fileAttr = File.GetAttributes(filepath + "/" + currentSelecteditemName);
+                if ((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    isfile = false;
+                }
+                else
+                {
+                    isfile = true;
+                }
+            }
+            catch (Exception t)
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            File_dt_time.Start();
+            File_path_tb.Text = getDrivePath();
+            loadfilesAction();
+            File_path_tb.Text = getDrivePath();
+        }
+
+        private void listView2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+             loadfilesAction();
+        }
+
+        private void listView2_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            string extention = Path.GetExtension(File_path_tb.Text + currentSelecteditemName);
+
+            if (extention == ".pdf" || extention == ".docx") {
+                // create array or collection for all selected items
+                var items = new List<ListViewItem>();
+                // add dragged one first
+                items.Add((ListViewItem)e.Item);
+                // optionally add the other selected ones
+                foreach (ListViewItem lvi in listView3.SelectedItems)
+                {
+                    if (!items.Contains(lvi))
+                    {
+                        items.Add(lvi);
+                    }
+                }
+                // pass the items to move...
+                listView3.DoDragDrop(items, DragDropEffects.Move);
+            }
+
+        }
+
+        private void listView3_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(List<ListViewItem>)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+        }
+
+        private void listView3_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(List<ListViewItem>)))
+            {
+                var items = (List<ListViewItem>)e.Data.GetData(typeof(List<ListViewItem>));
+                // move to dest LV
+                foreach (ListViewItem lvi in items)
+                {
+                    // LVI obj can only belong to one LVI, remove
+                    lvi.ListView.Items.Remove(lvi);
+                    listView3.Items.Add(lvi);
+                }
+            }
+        }
+
+
+        private void listView3_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            String itemname = e.Item.Text;
+            string itemTag = e.Item.Tag.ToString().Remove(2,2);
+            
+        }
+
+        private void listView2_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            Debug.WriteLine("Selected");
+        }
+
+        //GO BACK
+        private void removeStach()
+        {
+            string path = File_path_tb.Text;
+            if (path.LastIndexOf("/") == path.Length -1)
+            {
+                File_path_tb.Text = path.Substring(0, path.Length - 2);
+            }
+        }
+        private void back_bt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                removeStach();
+                String path = File_path_tb.Text;
+                path = path.Substring(0, path.LastIndexOf("/"));
+                File_path_tb.Text = path;
+                loadfilesAction();
+            }
+            catch (Exception t)
+            {
+
+            }
+        }
+    }
+}
